45be17ee674c7a40dcf224f0c1e83c705b74e093
Carlo Bilbao <cbilbao88@gmail.com>
2018-08-01 10:24:48 -0700

    add nutrient item component and test

---
+++ src/components/NutrientItem/NutrientItem.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import classes from './NutrientItem.css';
+
+const NutrientItem = (props) => {
+    return (
+        <input 
+            className={classes.input + " m-1 p-1 text-center rounded"} 
+            type="integer" 
+            placeholder={props.placeholder}
+            data-test="component-nutrient-item"/>
+    );
+}
+
+NutrientItem.propTypes = {
+    placeholder: PropTypes.string.isRequired
+}
+
+export default NutrientItem;
---
+++ src/components/NutrientItem/NutrientItem.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import { findByTestAttr, checkProps } from '../../utils/testUtils';
+
+import NutrientItem from './NutrientItem';
+
+import Enzyme from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ 
+    adapter: new EnzymeAdapter()
+});
+
+const defaultProps = {
+    placeholder: "Calcium (mg)"
+}
+
+const setup = (props={}) => {
+    const setupProps = { ...defaultProps, ...props };
+    return shallow(<NutrientItem {...setupProps}/>)
+}
+
+describe('component', ()=> {
+
+    let component;
+    beforeEach(() => {
+        const wrapper = setup();
+        component = findByTestAttr(wrapper, 'component-nutrient-item');
+    });
+    test('renders without error', () => {
+        expect(component.length).toBe(1);
+    });
+
+    test('renders without error with appropriate props', () => {
+        const expectedProps = { placeholder: "Lithium" };
+        checkProps(NutrientItem, expectedProps);
+    }) 
+})
