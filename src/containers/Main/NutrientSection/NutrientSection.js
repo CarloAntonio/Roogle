1832e668426657eb6d04de639e457fc6df3923c6
Carlo Bilbao <cbilbao88@gmail.com>
2018-08-01 20:33:48 -0700

    add test for nutrient section

--- src/containers/Main/NutrientSection/NutrientSection.js
+++ src/containers/Main/NutrientSection/NutrientSection.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import NutrientItem from './NutrientItem/NutrientItem';
 import classes from '../NutrientSection/NutrientSection.css';
 
 const NutrientSection = (props) => {
     return (
-        <section className="container">
+        <section className="container" data-test="component-nutrient-section">
 
             <div className="d-flex justify-content-center">
                 <h4 className={classes.subtitle + " px-4 border-bottom"}>Max Nutrients</h4>
@@ -16,13 +17,22 @@ const NutrientSection = (props) => {
                 {props.nutrientOptions.map(option => {
                     return <NutrientItem 
                         key={option.placeholder}
-                        placeholder={option.placeholder}/>
+                        placeholder={option.placeholder}
+                        data-test="nutrient-option"/>
                 })}
 
             </div>
 
         </section>
     );
+}
+
+NutrientSection.propTypes = {
+    nutrientOptions: PropTypes.arrayOf(
+        PropTypes.shape({
+            placeholder: PropTypes.string.isRequired
+        })
+    ).isRequired
 }
 
 export default NutrientSection;
---
+++ src/containers/Main/NutrientSection/NutrientSection.test.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import { findByTestAttr, checkProps } from '../../../utils/testUtils';
+
+import NutrientSection from './NutrientSection';
+
+import Enzyme from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ 
+    adapter: new EnzymeAdapter()
+});
+
+const defaultProps = {
+    nutrientOptions: []
+}
+const setup = (props={}) => {
+    const setupProps = { ...defaultProps, ...props };
+    return shallow(<NutrientSection {...setupProps}/>)
+}
+
+describe('component', ()=> {
+
+    let component;
+    beforeEach(() => {
+        const wrapper = setup();
+        component = findByTestAttr(wrapper, 'component-nutrient-section');
+    });
+
+    test('renders without error', () => {
+        expect(component.length).toBe(1);
+    });
+
+    test('renders without error with appropriate props', () => {
+        const expectedProps = { 
+            nutrientOptions: [{ placeholder: "Lithium" }]
+        };
+        checkProps(NutrientSection, expectedProps);
+    });
+
+});
+
+describe("component's subcomponents", () => {
+
+    let wrapper;
+    beforeEach(() => {
+        const inputProps = { 
+            nutrientOptions: [
+                { placeholder: "Lithium" }, 
+                { placeholder: "Lithium" },
+                { placeholder: "Lithium" },
+            ]
+        };
+        wrapper = setup(inputProps);
+    });
+    test('renders correct number of health selection items', () => {
+        const component = findByTestAttr(wrapper, 'nutrient-option');
+        expect(component.length).toBe(3);
+    });
+});
