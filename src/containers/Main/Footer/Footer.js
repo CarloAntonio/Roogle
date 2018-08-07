0f4d4d1a15185e1777b7d06fba9ad7926423a116
Carlo Bilbao <cbilbao88@gmail.com>
2018-08-06 17:28:53 -0700

    add edamam attribution

--- src/containers/Main/Footer/Footer.js
+++ src/containers/Main/Footer/Footer.js
@@ -4,8 +4,9 @@ import classes from './Footer.css';
 
 const Footer = () => {
     return (
-        <footer className={classes.styles + " p-3 text-center"} data-test="component-footer">
-            Copyright &copy; <a href="https://www.carlobilbao.com" target="_blank" rel="noopener noreferrer">Carlo Bilbao</a>
+        <footer className={classes.styles + " p-3 text-center d-flex flex-column"} data-test="component-footer">
+            <p>Copyright &copy; <a href="https://www.carlobilbao.com" target="_blank" rel="noopener noreferrer">Carlo Bilbao</a></p>
+            <div id="edamam-badge" data-color="white"></div>
         </footer>
     )
 }
